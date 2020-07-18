f7deaa23dd7a5c1fd59e0c129557a1d9a0a2eeb3
Aman Bilaiya <2018csb1069@iitrpr.ac.in>
2020-07-18 11:08:00 +0530

    Add files via upload

---
+++ line_follower_obstacle_detection.ino
@@ -0,0 +1,109 @@
+int MOTOR_A1 = 7;
+int MOTOR_B1 = 5;   
+int stop_distance=14;
+const int trigPin = 11;
+  const int echoPin = 12;
+  const int motorBspeed  =5;
+  long duration;
+  int distance;
+  int vSpeed = 110;                      
+  int turn_speed = 230;       // 0 - 255  max
+  int t_p_speed = 125;
+  int turn_delay = 10;
+
+int RIGHT = A0; 
+int LEFT = A1;
+int a,b;
+void setup() {
+  
+pinMode(MOTOR_A1, OUTPUT); 
+//pinMode(MOTOR_A2, OUTPUT); 
+pinMode(MOTOR_B1, OUTPUT); 
+//pinMode(MOTOR_B2, OUTPUT); 
+pinMode(RIGHT, INPUT); 
+pinMode(LEFT, INPUT);
+
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+Serial.begin(9600);
+}
+void loop() {
+  Serial.print(" \n");
+  a=analogRead(LEFT);
+   b=analogRead(RIGHT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  Serial.print(distance);
+   if(distance < stop_distance)
+   {
+     digitalWrite(MOTOR_A1, LOW);
+ // digitalWrite(MOTOR_A2, LOW);
+  //stop left motors
+  digitalWrite(MOTOR_B1, LOW);
+ // digitalWrite(MOTOR_B2, LOW);
+    Serial.print("stop\n");
+  while(distance<stop_distance)
+  {
+    digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH );
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+ // Serial.print(distance);
+  }
+   }
+if (analogRead(RIGHT)<=50 && analogRead(LEFT)<=50) //compare the both sensor to decide the direction
+{
+  //MOVE FORWARD//
+  
+  Serial.print("yes forward\n");
+  //run right motors clockwise
+  digitalWrite(MOTOR_A1, HIGH);
+  //digitalWrite(MOTOR_A2, HIGH);
+  //run left motors clockwise
+  digitalWrite(MOTOR_B1, HIGH);
+ // digitalWrite(MOTOR_B2, LOW);
+}
+else if (analogRead(RIGHT)<=50 && analogRead(LEFT)>=900) //compare the both sensor to decide the direction
+{
+  //MOVE RIGHT//
+  
+  Serial.print("yes right\n");  
+  //run right motors clockwise
+  digitalWrite(MOTOR_A1, HIGH);
+ // digitalWrite(MOTOR_A2, HIGH);
+  //run left motors anti-clockwise
+  digitalWrite(MOTOR_B1, LOW);
+ // digitalWrite(MOTOR_B2, HIGH);
+}
+else if (analogRead(RIGHT)>=900 && analogRead(LEFT)<=50) //compare the both sensor to decide the direction
+{ 
+  //MOVE-LEFT//
+  Serial.print("yes Left\n");
+  //run right motors anti-clockwise
+  digitalWrite(MOTOR_A1, LOW);
+ // digitalWrite(MOTOR_A2, LOW);
+  //run left motors clockwise
+  digitalWrite(MOTOR_B1, HIGH);
+ // digitalWrite(MOTOR_B2, LOW);
+}
+else if(analogRead(RIGHT)>=900 && analogRead(LEFT)>=900) //compare the both sensor to decide the direction
+{ 
+  //STOP//
+Serial.print("yes stop\n");
+
+  //stop right motors
+  digitalWrite(MOTOR_A1, LOW);
+ // digitalWrite(MOTOR_A2, LOW);
+  //stop left motors
+  digitalWrite(MOTOR_B1, LOW);
+ // digitalWrite(MOTOR_B2, LOW);
+  }
+}
